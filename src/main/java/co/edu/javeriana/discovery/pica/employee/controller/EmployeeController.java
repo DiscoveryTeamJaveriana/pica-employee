9bf0401f9d80737fd652b52afb6d1916ac0138c0
Beta993 <81639095+Beta993@users.noreply.github.com>
2021-11-12 20:27:30 -0500

    :sparkles: Se agrega trazabilidad Zipkin

--- src/main/java/co/edu/javeriana/discovery/pica/employee/controller/EmployeeController.java
+++ src/main/java/co/edu/javeriana/discovery/pica/employee/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package co.edu.javeriana.discovery.pica.employee.controller;
 import co.edu.javeriana.discovery.pica.employee.controller.model.ReqPostEmpleado;
 import co.edu.javeriana.discovery.pica.employee.controller.model.RespGetEmpleado;
 import co.edu.javeriana.discovery.pica.employee.service.IEmployeeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +21,17 @@ import java.util.ArrayList;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EmployeeController {
 
-    private static final String RQUID = "X-RqUID";
+    private static final String XRQUID = "X-RqUID";
     private static final String CODIGO = "Codigo";
+    private static final String REQUEST = "Request";
+    private static final String RESPONSE = "Response";
+    private static final String RESPONSECODE = "ResponseCode";
+    private static final String RQUID = "RqUID";
 
     private IEmployeeService employeeService;
+
+    @Autowired
+    private Tracer tracer;
 
     @Autowired
     public EmployeeController(IEmployeeService employeeService) {
@@ -30,23 +40,38 @@ public class EmployeeController {
 
 
     @PostMapping("/Empleado")
-        public ResponseEntity<Void> postEmpleado(@RequestBody ReqPostEmpleado reqPostEmpleado, @RequestHeader(value=RQUID) String xRqUID ) {
+        public ResponseEntity<Void> postEmpleado(@RequestBody ReqPostEmpleado reqPostEmpleado, @RequestHeader(value=XRQUID) String xRqUID ) throws JsonProcessingException {
         log.info("Creating Employee for RqUID {}", xRqUID);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(reqPostEmpleado);
+        EmployeeController.this.tracer.currentSpan().tag(REQUEST,json);
+        EmployeeController.this.tracer.currentSpan().tag(RQUID,xRqUID);
         employeeService.postEmpleado(reqPostEmpleado, xRqUID);
+        EmployeeController.this.tracer.currentSpan().tag(RESPONSECODE,HttpStatus.CREATED.toString());
         return new ResponseEntity<>(putRqUIDHeader(xRqUID),HttpStatus.CREATED);
     }
 
     @GetMapping("/Empleados")
-    public ResponseEntity< ArrayList<RespGetEmpleado>> getEmpleados(@RequestHeader(value=RQUID) String xRqUID ) {
+    public ResponseEntity< ArrayList<RespGetEmpleado>> getEmpleados(@RequestHeader(value=XRQUID) String xRqUID ) throws JsonProcessingException {
         log.info("Get Employees for RqUID {}", xRqUID);
+        EmployeeController.this.tracer.currentSpan().tag(RQUID,xRqUID);
         ArrayList<RespGetEmpleado> response = employeeService.getEmpleados(xRqUID);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(response);
+        EmployeeController.this.tracer.currentSpan().tag(RESPONSE,json);
+        EmployeeController.this.tracer.currentSpan().tag(RESPONSECODE,HttpStatus.OK.toString());
         return new ResponseEntity<>(response, putRqUIDHeader(xRqUID), HttpStatus.OK);
     }
 
     @GetMapping("/Empleado/{Codigo}")
-    public ResponseEntity<RespGetEmpleado> getEmpleado(@RequestHeader(value=RQUID) String xRqUID, @PathVariable(CODIGO) String codigo) {
+    public ResponseEntity<RespGetEmpleado> getEmpleado(@RequestHeader(value=XRQUID) String xRqUID, @PathVariable(CODIGO) String codigo) throws JsonProcessingException {
         log.info("Get Employee for RqUID {}", xRqUID);
+        EmployeeController.this.tracer.currentSpan().tag(RQUID,xRqUID);
         RespGetEmpleado response = employeeService.getEmpleado(codigo, xRqUID);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(response);
+        EmployeeController.this.tracer.currentSpan().tag(RESPONSE,json);
+        EmployeeController.this.tracer.currentSpan().tag(RESPONSECODE,HttpStatus.OK.toString());
         return new ResponseEntity<>(response,putRqUIDHeader(xRqUID),HttpStatus.OK);
     }
 
